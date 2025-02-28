20f4a3c33ee89e4ffe31d2165006f7619d995727
saipatibandla <sdpa14@gmail.com>
2025-02-28 14:30:39 -0600

    clean up

--- features/step_definitions/student_course_registration_steps.rb
+++ features/step_definitions/student_course_registration_steps.rb
@@ -6,18 +6,10 @@ Then('I should see the registration form') do
   expect(page).to have_selector('form')
 end
 
-# Then('I should see {string}') do |text|
-#   expect(page).to have_content(text)
-# end
-
-# Then('I should see the "Register" button') do
-#   expect(page).to have_button('Register')
-# end
-
 When('I select {string} for {string}') do |value, field|
   case field
   when "Year"
-    choose(value)  # Extracts "1" or "2" from "Year 1" or "Year 2"
+    choose(value)
   when "Math Course"
     select(value, from: 'math_course')
   when "Science Course"
@@ -32,7 +24,3 @@ end
 When('I click on the "Register" button') do
   click_button('Register')
 end
-
-# Then('I should see {string}') do |text|
-#   expect(page).to have_content(text)
-# end
