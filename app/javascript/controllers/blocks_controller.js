dedbe9087219fe7b42422b22b8772555fd238dd2
Zengqi Liu <cr400bfc5145@gmail.com>
2025-03-08 21:45:00 -0600

    Model views finalled

--- app/javascript/controllers/blocks_controller.js
+++ app/javascript/controllers/blocks_controller.js
@@ -7,7 +7,8 @@ export default class extends Controller {
     const checkboxes = document.querySelectorAll('.block input[type="checkbox"]')
     const modalOverlay = document.getElementById('modal-overlay')
     const modal = document.getElementById('modal')
-    const modalMessage = document.getElementById('modal-message')
+    const modalMessage = document.getElementById('modal-message');
+    const modalBlockContent = document.getElementById('modal-block-content')
     const modalSelectAgain = document.getElementById('modal-select-again')
     const maxSelectedBlocks = 1
 
@@ -47,21 +48,41 @@ export default class extends Controller {
     disableMoreSelections()
 
     selectBlockButton.addEventListener('click', (event) => {
-      event.preventDefault()
-      const selectedBlocks = []
+      event.preventDefault();
+      const selectedBlocks = [];
       checkboxes.forEach(checkbox => {
-        if (checkbox.checked) { selectedBlocks.push(checkbox.value) }
-      })
-
+        if (checkbox.checked) {
+          const blockEl = checkbox.closest('.block');
+          selectedBlocks.push(blockEl);
+        }
+      });
+    
       if (selectedBlocks.length > 0) {
-        const message = selectedBlocks.length === 1
-          ? `You have successfully selected <strong>Block ${selectedBlocks[0]}</strong>.`
-          : `You have successfully selected <strong>Blocks ${selectedBlocks.join(', ')}</strong>.`
-        modalMessage.innerHTML = message
-        modal.style.display = 'block'
-        modalOverlay.style.display = 'block'
+        let message;
+        if (selectedBlocks.length === 1) {
+          const blockCount = selectedBlocks[0].querySelector('input[type="checkbox"]').value;
+          message = `You have successfully selected <strong>Block ${blockCount}</strong>.`;
+        } else {
+          const blockNumbers = Array.from(selectedBlocks).map(block => 
+            block.querySelector('input[type="checkbox"]').value
+          );
+          message = `You have successfully selected <strong>Blocks ${blockNumbers.join(', ')}</strong>.`;
+        }
+        modalMessage.innerHTML = message;
+        modalBlockContent.innerHTML = "";
+    
+        selectedBlocks.forEach(blockEl => {
+          const clone = blockEl.cloneNode(true);
+          const header = clone.querySelector('.block-title');
+          if (header) { header.remove(); }
+          modalBlockContent.appendChild(clone);
+        });
+  
+        modal.style.display = 'block';
+        modalOverlay.style.display = 'block';
       }
-    })
+    });
+    
 
     modalSelectAgain.addEventListener('click', () => {
       modal.style.display = 'none'
