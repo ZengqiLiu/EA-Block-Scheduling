6942d19da7d329cb57c77fd90a70b94ab5cd4801
Zengqi Liu <cr400bfc5145@gmail.com>
2025-04-15 22:38:46 -0500

    Controller logic added for select 0 or more than 1 blocks

--- app/controllers/block_selections_controller.rb
+++ app/controllers/block_selections_controller.rb
@@ -2,10 +2,28 @@ class BlockSelectionsController < ApplicationController
   before_action :ensure_student
 
   def create
-    block = Block.new(block_params[:courses])
+    selected_ids = params.dig(:block, :selected_block_ids)
+    if selected_ids.blank?
+      flash[:alert] = "No block was selected. Please select a block."
+      redirect_back fallback_location: blocks_path and return
+    elsif selected_ids.size > 1
+      flash[:alert] = "Please only select one block."
+      redirect_back fallback_location: blocks_path and return
+    end
+ 
+    selected_block_id = selected_ids.first.to_s
+    courses_hash = params[:block][:courses] || {}
+    courses_for_selected = courses_hash[selected_block_id]
 
-    if block.valid?
-      current_user.create_block_selection!(course_ids: block.courses.map(&:id))
+    if courses_for_selected.blank?
+      flash[:alert] = "No courses found for the selected block."
+      redirect_back fallback_location: blocks_path and return
+    end
+
+    block_instance = Block.new(courses_for_selected)
+
+    if block_instance.valid?
+      current_user.create_block_selection!(course_ids: block_instance.courses.map(&:id))
       flash[:notice] = "Block successfully selected."
       redirect_back fallback_location: blocks_path
     else
@@ -21,6 +39,6 @@ class BlockSelectionsController < ApplicationController
   end
 
   def block_params
-    params.require(:block).permit(courses: [])
+    params.require(:block).permit(courses: {}, selected_block_ids: [])
   end
 end
